1d991209e9a2718f3327590dca450a11fb854977
dong_hy <kimadonghyon99@gmail.com>
2024-06-15 19:17:07 +0900

    feat : ChatMessageDto 수정 1. 수신자 이메일 제거 -> 세션에서 흭득

--- src/main/java/org/chatapp/chatapp/common/websocket/ChatMessageDto.java
+++ src/main/java/org/chatapp/chatapp/common/websocket/ChatMessageDto.java
@@ -20,7 +20,6 @@ public class ChatMessageDto {
 
     private MessageType messageType; // 메시지 타입
     private Long chatRoomId; // 방 번호
-    private Long senderEmail; // 채팅을 보낸 사람
     private String message; // 메시지
     private List<String> receiverEmail; // 수신자 이메일
 }
