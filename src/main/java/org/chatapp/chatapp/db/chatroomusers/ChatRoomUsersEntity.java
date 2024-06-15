a8c3ddd8ece1d8cb689eeb1bd6fc24d885cbdcef
dong_hy <kimadonghyon99@gmail.com>
2024-06-15 19:13:40 +0900

    feat : ChatRoomUsersEntity 관계설정 1. chatRoomEntity을 부모 테이블로가진다.

--- src/main/java/org/chatapp/chatapp/db/chatroomusers/ChatRoomUsersEntity.java
+++ src/main/java/org/chatapp/chatapp/db/chatroomusers/ChatRoomUsersEntity.java
@@ -1,6 +1,9 @@
 package org.chatapp.chatapp.db.chatroomusers;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -9,6 +12,7 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.chatapp.chatapp.db.BaseEntity;
+import org.chatapp.chatapp.db.chatroom.ChatRoom;
 
 @Data
 @NoArgsConstructor
@@ -19,6 +23,9 @@ import org.chatapp.chatapp.db.BaseEntity;
 @Table(name = "chat_room_users")
 public class ChatRoomUsersEntity extends BaseEntity {
     private Long userId;
-    private Long chatRoomId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatRoomId")
+    private ChatRoom chatRoom;
     private LocalDateTime joinedAt;
 }
